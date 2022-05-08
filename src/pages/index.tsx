5d7c5bd2df44c3c50a7e1002bb3d70da7e054210
Karen <ksuen.dev@gmail.com>
2022-05-09 00:58:56 +0100

    🐛 FIX Inter font not displayed properly, use official CDN
    ✅ Import Inter from official CDN instead of outdated Google Fonts API
    ✅ Inter stylistic set (OpenType feature) now displays properly on all supported browsers and devices

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,14 +5,14 @@ const Home: NextPage = () => {
   return (
     <div className="h-screen">
       <Head>
-        <title>Mining PSU Calculator</title>
+        <title>Mining Rig Power Calculator</title>
         <link rel="icon" href="/public/favicon.ico" />
       </Head>
-      <main className="flex items-center justify-center h-full">
+      <main className="flex flex-col items-center justify-center h-full">
         <h1
           className="text-5xl leading-tight font-bold text-transparent bg-clip-text bg-gradient-to-b from-orange-light to-orange-dark title-shadow tracking-[-0.01em]">Mining
-          PSU
-          Calculator</h1>
+          Rig Power Calculator</h1>
+
       </main>
     </div>
   );
